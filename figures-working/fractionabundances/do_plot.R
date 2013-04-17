8a2bed936ae9113d5143323ee9cda662c4077143
David Wilkins <david@wilkox.org>
2013-04-17 13:58:41 +1000

    Working on PF; illustrating relative abundance problem

---
+++ figures-working/fractionabundances/do_plot.R
@@ -0,0 +1,63 @@
+#library
+library(ggplot2)
+
+#values
+df <- data.frame(fraction = factor(c("0.1 µm", "0.1 µm", "0.8 µm", "0.8 µm", "3.0 µm", "3.0 µm")),
+                levels = c("0.1 µm", "0.8 µm", "3.0 µm"),
+                OTU = factor(c("OTU 1", "OTU 2", "OTU 1", "OTU 2", "OTU 1", "OTU 2")),
+                abundance = c(1, 49, 3, 71, 8, 10)
+                )
+
+#plot
+p <- ggplot(data=df, aes(x=fraction, y=abundance, fill=OTU))
+p <- p + geom_bar(stat="identity")
+p <- p + theme(panel.grid.major=element_blank(), panel.grid.minor = element_blank(), panel.background=element_blank())
+p <- p + theme(panel.border=element_rect(fill=NA))
+p <- p + theme(axis.title.x=element_blank())
+p <- p + theme(axis.text.x = element_text(color="black"))
+p <- p + theme(axis.ticks.x = element_blank())
+p <- p + ylab("Number of cells")
+p <- p + xlab("Size fraction")
+p <- p + theme(legend.position="none")
+
+#output
+ggsave("fractionabundances1.png", p, width=3, height=3, dpi=1200)
+
+#values
+df <- data.frame(fraction = factor(c("0.1 µm", "0.1 µm", "0.8 µm", "0.8 µm", "3.0 µm", "3.0 µm")),
+                levels = c("0.1 µm", "0.8 µm", "3.0 µm"),
+                OTU = factor(c("OTU 1", "OTU 2", "OTU 1", "OTU 2", "OTU 1", "OTU 2")),
+                abundance = c(2, 98, 4, 96, 44, 56)
+                )
+
+#plot
+p <- ggplot(data=df, aes(x=fraction, y=abundance, fill=OTU))
+p <- p + geom_bar(stat="identity")
+p <- p + theme(panel.grid.major=element_blank(), panel.grid.minor = element_blank(), panel.background=element_blank())
+p <- p + theme(panel.border=element_rect(fill=NA))
+p <- p + theme(axis.title.x=element_blank())
+p <- p + theme(axis.text.x = element_text(color="black"))
+p <- p + theme(axis.ticks.x = element_blank())
+p <- p + ylab("Relative abundance (%)")
+p <- p + xlab("Size fraction")
+p <- p + theme(legend.position="none")
+
+#output
+ggsave("fractionabundances2.png", p, width=3, height=3, dpi=1200)
+
+#values
+df <- data.frame(OTU = factor(c("OTU 1", "OTU 2")), abundance = c(50, 50), sample=factor(c("Combined", "Combined")), levels=c("Combined"))
+
+#plot
+p <- ggplot(data=df, aes(y=abundance, fill=OTU, x=sample))
+p <- p + geom_bar(stat="identity")
+p <- p + theme(panel.grid.major=element_blank(), panel.grid.minor = element_blank(), panel.background=element_blank())
+p <- p + theme(panel.border=element_rect(fill=NA))
+p <- p + theme(axis.title.x=element_blank())
+p <- p + theme(axis.text.x = element_text(color="black"))
+p <- p + theme(axis.ticks.x = element_blank())
+p <- p + ylab("Relative abundance (%)")
+p <- p + theme(legend.position="none")
+
+#output
+ggsave("fractionabundances3.png", p, width=1.7, height=3, dpi=1200)
